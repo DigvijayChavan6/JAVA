81bf02791049bb93809a823803c226086017bfc4
Digvijay Chavan <chavandigvijay9081@gmail.com>
2023-08-26 22:43:57 +0530

    basic program

---
+++ Vari.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Vari{
+    public static void main(String[] args) {
+        // long nextInt=Long.MAX_VALUE;
+        // Scanner sc = new Scanner (System.in);
+        // int Scanner=0;
+        // System.out.println("Enter any number");
+        // int va=sc.nextInt();
+        
+        // System.out.println(nextInt);
+        // System.out.println(Scanner);
+        float f=0.3F;
+        double dd=1.2D;
+        // int d=00103;
+        int d=10,c=5;
+        boolean b=true;
+        System.out.println(f);
+        System.out.println(++d+(++c));
+    }
+}
